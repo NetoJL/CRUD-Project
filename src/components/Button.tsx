23c46b7fc5e869af768daa15401a0047b82221b1
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-30 13:14:28 -0300

    Button component created and updated

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps {
+    buttonColor?: 'green' | 'blue' | 'gray'
+    className?: string
+    children: any
+}
+
+export default function Button(props: ButtonProps) {
+
+    const color = props.buttonColor ?? 'gray'
+
+    return (
+        <button className={`
+            bg-gradient-to-r from-${color}-400 to-${color}-700
+            text-white px-4 py-2 rounded-md
+            ${props.className}
+        `}>
+            {props.children}
+        </button>
+    )
+}
