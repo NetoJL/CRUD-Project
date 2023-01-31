201c8808de1be9a368434a64fcf8745fafcb332e
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-31 13:26:41 -0300

    index.tsx updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Customer from "@/core/Customer";
 import Table from "@/components/Table"
 import Button from "@/components/Button";
 import Form from "@/components/Form";
+import { useState } from "react";
 
 export default function Home() {
 
@@ -13,31 +14,50 @@ export default function Home() {
     new Customer('Carlos', 33, '4'),
   ]
 
+  const [visible, setVisible] = useState<'table' | 'form'>('table')
+  const [customer, setCustomer] = useState<Customer>(Customer.empty())
+
   function selectedCustomer(customer: Customer) {
-    alert(customer.name)
+    setCustomer(customer)
+    setVisible('form')
   }
 
   function deletedCustomer(customer: Customer) {
     alert('Delete ' + customer.name)
   }
 
+  function SaveCustomer(customer: Customer) {
+    console.log(customer)
+    setVisible('table')
+  }
+
+  function newCustomer() {
+    setCustomer(Customer.empty())
+    setVisible('form')
+  }
+
   return (
-    <div className={`
+    <div className="
     flex justify-center items-center h-screen
     bg-gradient-to-r from-blue-500 to-purple-500
     text-white
-    `}>
+    ">
       <Layout title="Sign up">
-        <div className="flex justify-end">
-          <Button buttonColor="green" className="mb-4">
-            New Customer
-          </Button>
-        </div>
-        {/* <Table
-          customers={customers}
-          selectedCustomer={selectedCustomer}
-          deletedCustomer={deletedCustomer} /> */}
-          <Form customer={customers[3]}/>
+        {visible === 'table' ? (
+          <>
+            <div className="flex justify-end">
+              <Button color="green" style="mb-4" onClick={newCustomer}>
+                New Customer
+              </Button>
+            </div>
+            <Table
+              customers={customers}
+              selectedCustomer={selectedCustomer}
+              deletedCustomer={deletedCustomer} />
+          </>
+        ) : (
+          <Form customer={customer} customerUpdated={SaveCustomer} cancelled={() => setVisible('table')}/>
+        )}
       </Layout>
     </div>
   )
